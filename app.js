25e0beac2471d8ab6eb149f90775b557ce1eae7e
tommyWrightWeb <thomas.wright.web@gmail.com>
2024-01-04 12:50:41 +0000

    change port number to http

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
+app.listen(80, () => {
     console.log('Serving on port 3000')
 })
 
