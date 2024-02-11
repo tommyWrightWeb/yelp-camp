93252fe685f9ad857d819c80bc6311d08e690721
tommyWrightWeb <114081909+tommyWrightWeb@users.noreply.github.com>
2024-02-11 13:18:26 +0000

    Update campground.js

--- models/campground.js
+++ models/campground.js
@@ -1,67 +1,69 @@
+// Importing necessary modules
 const mongoose = require('mongoose');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 
-
-// https://res.cloudinary.com/douqbebwk/image/upload/w_300/v1600113904/YelpCamp/gxgle1ovzd2f3dgcpass.png
-
+// Schema for the images associated with the campground
 const ImageSchema = new Schema({
-    url: String,
-    filename: String
+    url: String, // URL of the image
+    filename: String // Filename of the image
 });
 
+// Virtual for generating thumbnail URL of the image
 ImageSchema.virtual('thumbnail').get(function () {
-    return this.url.replace('/upload', '/upload/w_200');
+    return this.url.replace('/upload', '/upload/w_200'); // Generating thumbnail URL
 });
 
+// Options for virtuals in the schema
 const opts = { toJSON: { virtuals: true } };
 
+// Schema for the Campground model
 const CampgroundSchema = new Schema({
-    title: String,
-    images: [ImageSchema],
-    geometry: {
+    title: String, // Title of the campground
+    images: [ImageSchema], // Array of images associated with the campground
+    geometry: { // Geographic coordinates of the campground
         type: {
             type: String,
-            enum: ['Point'],
+            enum: ['Point'], // Type of coordinates (only 'Point' supported)
             required: true
         },
-        coordinates: {
+        coordinates: { // Latitude and longitude coordinates
             type: [Number],
             required: true
         }
     },
-    price: Number,
-    description: String,
-    location: String,
-    author: {
+    price: Number, // Price of the campground
+    description: String, // Description of the campground
+    location: String, // Location of the campground
+    author: { // Author of the campground (reference to User model)
         type: Schema.Types.ObjectId,
         ref: 'User'
     },
-    reviews: [
+    reviews: [ // Reviews associated with the campground (reference to Review model)
         {
             type: Schema.Types.ObjectId,
             ref: 'Review'
         }
     ]
-}, opts);
+}, opts); // Applying options for virtuals
 
-
+// Virtual property for generating popup markup for map markers
 CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
     return `
-    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
-    <p>${this.description.substring(0, 20)}...</p>`
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong> 
+    <p>${this.description.substring(0, 20)}...</p>`; // Generating popup markup
 });
 
-
-
+// Middleware to delete associated reviews when a campground is deleted
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc) {
-        await Review.deleteMany({
+        await Review.deleteMany({ // Deleting associated reviews
             _id: {
-                $in: doc.reviews
+                $in: doc.reviews // Finding reviews by their IDs
             }
         })
     }
 })
 
+// Exporting the Campground model
 module.exports = mongoose.model('Campground', CampgroundSchema);
