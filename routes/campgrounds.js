7547a24b095ff10510549598a10b2e7a8090e708
tommyWrightWeb <114081909+tommyWrightWeb@users.noreply.github.com>
2024-02-11 13:20:38 +0000

    Update campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,28 +1,29 @@
+// Importing necessary modules
 const express = require('express');
 const router = express.Router();
-const campgrounds = require('../controllers/campgrounds');
-const catchAsync = require('../utils/catchAsync');
-const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
-const multer = require('multer');
-const { storage } = require('../cloudinary');
-const upload = multer({ storage });
+const campgrounds = require('../controllers/campgrounds'); // Importing campground controller methods
+const catchAsync = require('../utils/catchAsync'); // Importing utility function for error handling
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware'); // Importing middleware functions
+const multer = require('multer'); // Importing multer for file uploads
+const { storage } = require('../cloudinary'); // Importing cloudinary storage
+const upload = multer({ storage }); // Configuring multer for storage
 
-const Campground = require('../models/campground');
+const Campground = require('../models/campground'); // Importing Campground model
 
+// Routes for campgrounds
 router.route('/')
-    .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+    .get(catchAsync(campgrounds.index)) // Route for displaying all campgrounds
+    .post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground)); // Route for creating a new campground
 
-
-router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+// Route for displaying the form to create a new campground
+router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
-    .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
-    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+    .get(catchAsync(campgrounds.showCampground)) // Route for displaying a specific campground
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground)) // Route for updating a campground
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground)); // Route for deleting a campground
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
+// Route for displaying the form to edit a campground
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
 
-
-
-module.exports = router;
+module.exports = router; // Exporting the router
