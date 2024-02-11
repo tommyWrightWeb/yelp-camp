19252331cbb87d990b7f939384d5c0714e180a4d
tommyWrightWeb <114081909+tommyWrightWeb@users.noreply.github.com>
2024-02-11 13:21:17 +0000

    Update reviews.js

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,14 +1,17 @@
+// Importing necessary modules
 const express = require('express');
-const router = express.Router({ mergeParams: true });
-const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
-const Campground = require('../models/campground');
-const Review = require('../models/review');
-const reviews = require('../controllers/reviews');
-const ExpressError = require('../utils/ExpressError');
-const catchAsync = require('../utils/catchAsync');
+const router = express.Router({ mergeParams: true }); // Creating a router with merged parameters
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware'); // Importing middleware functions
+const Campground = require('../models/campground'); // Importing Campground model
+const Review = require('../models/review'); // Importing Review model
+const reviews = require('../controllers/reviews'); // Importing review controller methods
+const ExpressError = require('../utils/ExpressError'); // Importing utility function for handling errors
+const catchAsync = require('../utils/catchAsync'); // Importing utility function for error handling
 
-router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
+// Route for creating a new review
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
 
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
+// Route for deleting a review
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
 
-module.exports = router;
+module.exports = router; // Exporting the router
