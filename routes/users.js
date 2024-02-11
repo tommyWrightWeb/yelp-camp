20f62cf5dbece005e7d84878e2973846376a5074
tommyWrightWeb <114081909+tommyWrightWeb@users.noreply.github.com>
2024-02-11 13:21:54 +0000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,22 @@
+// Importing necessary modules
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const catchAsync = require('../utils/catchAsync');
-const User = require('../models/user');
-const users = require('../controllers/users');
+const catchAsync = require('../utils/catchAsync'); // Importing utility function for error handling
+const User = require('../models/user'); // Importing User model
+const users = require('../controllers/users'); // Importing user controller methods
 
+// Routes for user registration
 router.route('/register')
-    .get(users.renderRegister)
-    .post(catchAsync(users.register));
+    .get(users.renderRegister) // Route for rendering registration form
+    .post(catchAsync(users.register)); // Route for handling user registration
 
+// Routes for user login
 router.route('/login')
-    .get(users.renderLogin)
-    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
+    .get(users.renderLogin) // Route for rendering login form
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login); // Route for handling user login
 
-router.get('/logout', users.logout)
+// Route for user logout
+router.get('/logout', users.logout);
 
-module.exports = router;
+module.exports = router; // Exporting the router
