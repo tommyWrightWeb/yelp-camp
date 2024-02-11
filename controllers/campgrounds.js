e1dbb2024382c7f9efc8f3fe6da67dbbcf86864d
tommyWrightWeb <114081909+tommyWrightWeb@users.noreply.github.com>
2024-02-11 13:16:33 +0000

    Update campgrounds.js

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,24 +1,30 @@
+// Importing necessary modules and models
 const Campground = require('../models/campground');
 const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
 const mapBoxToken = process.env.MAPBOX_TOKEN;
 const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const { cloudinary } = require("../cloudinary");
 
-
+// Controller method to render the index page with all campgrounds
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({}).populate('popupText');
     res.render('campgrounds/index', { campgrounds })
 }
 
+// Controller method to render the form for creating a new campground
 module.exports.renderNewForm = (req, res) => {
     res.render('campgrounds/new');
 }
 
+// Controller method to handle the creation of a new campground
 module.exports.createCampground = async (req, res, next) => {
+    // Forward geocoding to get latitude and longitude from location string
     const geoData = await geocoder.forwardGeocode({
         query: req.body.campground.location,
         limit: 1
     }).send()
+    
+    // Creating a new campground with data from the request body
     const campground = new Campground(req.body.campground);
     campground.geometry = geoData.body.features[0].geometry;
     campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
@@ -29,6 +35,7 @@ module.exports.createCampground = async (req, res, next) => {
     res.redirect(`/campgrounds/${campground._id}`)
 }
 
+// Controller method to render the details of a specific campground
 module.exports.showCampground = async (req, res,) => {
     const campground = await Campground.findById(req.params.id).populate({
         path: 'reviews',
@@ -43,6 +50,7 @@ module.exports.showCampground = async (req, res,) => {
     res.render('campgrounds/show', { campground });
 }
 
+// Controller method to render the form for editing a campground
 module.exports.renderEditForm = async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id)
@@ -53,13 +61,16 @@ module.exports.renderEditForm = async (req, res) => {
     res.render('campgrounds/edit', { campground });
 }
 
+// Controller method to handle the updating of a campground
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
     console.log(req.body);
+    // Finding the campground by ID and updating its data with data from the request body
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
     await campground.save();
+    // Deleting images if requested
     if (req.body.deleteImages) {
         for (let filename of req.body.deleteImages) {
             await cloudinary.uploader.destroy(filename);
@@ -70,8 +81,10 @@ module.exports.updateCampground = async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 }
 
+// Controller method to handle the deletion of a campground
 module.exports.deleteCampground = async (req, res) => {
     const { id } = req.params;
+    // Finding the campground by ID and deleting it
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Successfully deleted campground')
     res.redirect('/campgrounds');
